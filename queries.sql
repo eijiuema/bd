9c4a0b4221e353304f179338cf50bbe9eed87908
Eiji Uema <eijiuema@gmail.com>
2019-11-16 19:46:55 -0300

    queries 3 e 4 adicionadas

---
+++ queries.sql
@@ -0,0 +1,22 @@
+-- 1
+
+
+-- 2
+
+
+-- 3
+SELECT setor, COUNT(*) FROM enfermeiro GROUP BY setor;
+
+-- 4
+SELECT especialidade, COUNT(*) FROM medico GROUP BY especialidade;
+
+-- 5
+
+
+-- 6
+
+
+-- 7
+
+
+-- 8
